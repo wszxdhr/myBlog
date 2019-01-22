b6022dc1408eee663d19241c0b76b81697a6a666
wszxdhr <wszxdhr@126.com>
2019-01-22 08:02:01 +0800

    feat: add markdown renderer

--- pages/_platform/article/_id.vue
+++ pages/_platform/article/_id.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="article-container">
-    {{ $route.params.id }}
+    <div
+      class="article-markdown"
+      v-html="content"/>
   </div>
 </template>
 
 <script>
+import {markdown} from 'markdown'
 export default {
   name: 'ArticleContainer',
   data: () => ({
-    markdown: ''
+    articleData: {},
+    content: '',
+    contentTree: {}
   }),
   created () {
     this.$axios.getArticle({id: this.$route.params.id}).then(res => {
       let resData = res.data
       if (res.error || !resData) {} else {
-        
+        this.articleData = resData
+        this.content = markdown.toHTML(resData.content)
+        this.contentTree = markdown.parse(resData.content)
       }
-      console.log(resData)
     })
   }
 }
