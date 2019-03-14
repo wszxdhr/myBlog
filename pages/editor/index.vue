95d8ab42cbee4aa4aa4272faf0530c7dbbc396f0
张潇 <wszxdhr@126.com>
2019-03-14 08:31:13 +0800

    feat:add editor

---
+++ pages/editor/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="container wide">
+    <el-form
+      ref="form"
+      :model="form"
+      label-width="80px">
+      <el-form-item label="文章ID">
+        <el-input
+          v-model="articleId"
+          placeholder="未输入文章ID，则为添加" >
+          <el-button
+            slot="append"
+            icon="el-icon-search" />
+        </el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input
+          v-model="form.password"
+          type="password" />
+      </el-form-item>
+      <el-form-item label="标题">
+        <el-input
+          v-model="form.title" />
+      </el-form-item>
+      <el-form-item label="标签">
+        <el-input
+          v-model="form.tags" />
+      </el-form-item>
+      <el-form-item label="描述">
+        <el-input
+          v-model="form.description" />
+      </el-form-item>
+      <el-form-item label="分类">
+        <el-input
+          v-model="form.subject" />
+      </el-form-item>
+      <el-form-item label="头图">
+        <el-input
+          v-model="form.banner" />
+      </el-form-item>
+      <el-form-item label="正文">
+        <el-input
+          v-model="form.content"
+          :autosize="{ minRows: 4, maxRows: 20}"
+          type="textarea" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Editor',
+  data () {
+    return {
+      articleId: '',
+      form: {
+        password: '',
+        title: '',
+        tags: '',
+        content: '',
+        description: '',
+        subject: '',
+        banner: ''
+      }
+    }
+  }
+}
+</script>
